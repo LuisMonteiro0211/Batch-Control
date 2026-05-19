51bab0883d4d19c1bd84c2becc31b95b94b0d823
Luis Monteiro <luisfelipeomonteiro@gmail.com>
2026-05-19 18:50:22 -0300

    feat(gui): add frame de prodtos ao app
    Adiciona o módulo de ProductFrame ao app (src/gui/components/product_frame.py) e faz toda a configuração de layout do frame

--- src/gui/app.py
+++ src/gui/app.py
@@ -3,7 +3,7 @@ Módulo principal para construção da interface gráfica da aplicação.
 """
 
 from customtkinter import CTk
-from src.gui.components import Sidebar
+from src.gui.components import Sidebar, ProductFrame
 from src.gui.theme import COLORS, FONTS, SETTINGS
 from src.gui.app_controller import AppController
 
@@ -20,15 +20,22 @@ class BatchControlApp(CTk):
 
     def _build_widgets(self):
         self._create_sidebar()
-    
+        self._create_product_frame()
+
     def _create_sidebar(self):
         self._sidebar = Sidebar(self,
         on_click_product=self._controller.on_click_product_buttom, 
         on_click_batch=self._controller.on_click_batch_buttom)
 
+    def _create_product_frame(self):
+        self._product_frame = ProductFrame(self)
+
     def _layout_widgets(self):
         self._sidebar.pack(side="left", fill="y")
         self._sidebar.pack_propagate(False)
+
+        self._product_frame.pack(side="left")
+        self._product_frame.pack_propagate(False)
 
     def run(self):
         self.mainloop()
